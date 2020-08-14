6e17461979fc81a62d5c49b9838d4942da867f73
Brad Korman <brad.korman@remarque-creative.com>
2020-08-14 12:04:17 -0400

    working on styling

--- App.js
+++ App.js
@@ -1,21 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={{padding:30}}>
+      <View
+        style={{
+          flexDirection:'row',
+          justifyContent: 'space-between',
+          alignItems: 'center'
+        }}>
+        <TextInput
+          placeholder="Enter your goal"
+          style={{ width: '80%', borderColor:'black', borderWidth:1, padding:10 }}
+        />
+        <Button title="Add" />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
